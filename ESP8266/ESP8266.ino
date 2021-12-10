f3cd8aa576aa564cc0fff3c07f2a2b7b8c04830e
Johannes Huber <50733690+joehubi@users.noreply.github.com>
2021-12-10 10:43:33 +0100

    Add files via upload

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -18,20 +18,34 @@
 
 // #################################################################### Variables
 
-word debug_level = 0;
+word debug_level = 2;
+
+float dutycycle = 0.0;
+int dutycycle_int = 0;
 
 float TS01 = 0.0;
-int FS01 = 0;
+float TS02 = 0.0;
+float TS03 = 0.0;
+int TS01_int = 0;
+int TS02_int = 0;
+int TS03_int = 0;
 
 int _sends = 0;
 int _reads = 0;
 
+int word_hour = 0;
+int word_minute = 0;
+
 unsigned long millisec;           // time-ms
 unsigned long cycle_read_pre = 0;
-const unsigned long cycle_read_period = 1000;  
+const unsigned long cycle_read_period = 456;  
 unsigned long cycle_send_pre = 0;
-const unsigned long cycle_send_period = 10000;  
+const unsigned long cycle_send_period = 5000;  
 
+const int mcutimeout = 300;
+const int mcubaudrate = 1200;
+const int txbuffer = 150;
+const int rxbuffer = 500;
 SoftwareSerial nodemcu(D6, D5);           // Serial conncetion to NodeMCU
 
 unsigned long arduino_time_ms = 0;
@@ -39,11 +53,25 @@ unsigned long arduino_time_ms = 0;
 const char* ssid = "Pumuckel";            // wifi network
 const char* password = "Stiller_83";      // wifi network
 
-int dummy_state = 0;                      // 0=OFF, 1=ON
+byte dummy_state = 0;                      // 0=OFF, 1=ON
 String dummy_state_str = "...";
-
-int dummy_state_ctl = 0;                  // 0=Manual ON, 1=Manual OFF, 2=AUTOMATIC
+byte dummy_state_ctl = 2;                  // 0=Manual ON, 1=Manual OFF, 2=AUTOMATIC
 String dummy_state_ctl_str = "...";
+
+byte pipevent_state = 0;                      // 0=OFF, 1=ON
+String pipevent_state_str = "...";
+byte pipevent_state_ctl = 2;                  // 0=Manual ON, 1=Manual OFF, 2=AUTOMATIC
+String pipevent_state_ctl_str = "...";
+
+byte led_state = 0;                      // 0=OFF, 1=ON
+String led_state_str = "...";
+byte led_state_ctl = 2;                  // 0=Manual ON, 1=Manual OFF, 2=AUTOMATIC
+String led_state_ctl_str = "...";
+
+byte ventilator_state = 0;                      // 0=OFF, 1=ON
+String ventilator_state_str = "...";
+byte ventilator_state_ctl = 2;                  // 0=Manual ON, 1=Manual OFF, 2=AUTOMATIC
+String ventilator_state_ctl_str = "...";
 
 // Timer for timed loops
 int cycle_1500ms = 1000;      
@@ -59,10 +87,36 @@ AsyncWebServer server(80);                // Create AsyncWebServer object on por
 
 
 
-
-
-
 // #################################################################### Functions
+
+String state_ctl_txt(int ctl){
+  String state_ctl_txt = "";
+  switch (ctl) {
+    case 0:
+      state_ctl_txt = "Manual OFF";
+      break;
+    case 1:
+      state_ctl_txt = "Manual ON";
+      break;
+    case 2:
+      state_ctl_txt = "AUTOMATIC";
+      break;
+  }
+  return state_ctl_txt;
+}
+
+String state_txt(int state){
+  String state_txt = "";
+  switch (state) {
+    case 0:
+      state_txt = "OFF";
+      break;
+    case 1:
+      state_txt = "ON";
+      break;
+  }
+  return state_txt;
+}
 
 // Get-functions for collecting the data from the arduino and display it on the webserver
 String gettime() {
@@ -77,8 +131,20 @@ String getreads() {
 String getTS01() {
   return String(TS01);
 }
-String getFS01() {
-  return String(FS01);
+String getTS02() {
+  return String(TS02);
+}
+String getTS03() {
+  return String(TS03);
+}
+String getdutycycle() {
+  return String(dutycycle);
+}
+String getwordhour() {
+  return String(word_hour);
+}
+String getwordminute() {
+  return String(word_minute);
 }
 
 // Multiplex processor for all data variables 
@@ -90,12 +156,36 @@ String processor(const String& var){
   if(var == "dummy_state_ctl_str"){
     return dummy_state_ctl_str;
   }
+  if(var == "pipevent_state_str"){
+    return pipevent_state_str;
+  }
+  if(var == "pipevent_state_ctl_str"){
+    return pipevent_state_ctl_str;
+  }
+  if(var == "led_state_str"){
+    return led_state_str;
+  }
+  if(var == "led_state_ctl_str"){
+    return led_state_ctl_str;
+  }
+  if(var == "ventilator_state_str"){
+    return ventilator_state_str;
+  }
+  if(var == "ventilator_state_ctl_str"){
+    return ventilator_state_ctl_str;
+  }      
   else if (var == "TS01"){
     return getTS01();
   }
-  else if (var == "FS01"){
-    return getFS01();
+  else if (var == "TS02"){
+    return getTS02();
   }
+  else if (var == "TS03"){
+    return getTS03();
+  }
+  else if (var == "dutycycle"){
+    return getdutycycle();
+  }    
   else if (var == "arduino_time_ms"){
     return gettime();
   }
@@ -105,16 +195,21 @@ String processor(const String& var){
   else if (var == "_sends"){
     return getsends();
   }
+  else if (var == "word_hour"){
+    return getwordhour();
+  }
+  else if (var == "word_minute"){
+    return getwordminute();
+  }
 }
-
 
 
 // #################################################################### Setup
 
 void setup(){
 
-  nodemcu.setTimeout(20);
-  nodemcu.begin(1200);    // set low baud rate due to json communication
+  nodemcu.setTimeout(mcutimeout);
+  nodemcu.begin(mcubaudrate);    // set low baud rate due to json communication
 
   Serial.begin(9600);   // for serial output in window / debugging
    
@@ -165,7 +260,51 @@ void setup(){
     dummy_state_ctl = 2;
   });
 
+  // Pipe ventilator state Button
+  
+  server.on("/b2_off", HTTP_GET, [](AsyncWebServerRequest *request){  
+    pipevent_state_ctl = 0;
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  server.on("/b2_on", HTTP_GET, [](AsyncWebServerRequest *request){  
+    pipevent_state_ctl = 1;
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  }); 
+  server.on("/b2_auto", HTTP_GET, [](AsyncWebServerRequest *request){  
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+    pipevent_state_ctl = 2;
+  });
 
+  // LED state Button
+  
+  server.on("/b3_off", HTTP_GET, [](AsyncWebServerRequest *request){  
+    led_state_ctl = 0;
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  server.on("/b3_on", HTTP_GET, [](AsyncWebServerRequest *request){  
+    led_state_ctl = 1;
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  }); 
+  server.on("/b3_auto", HTTP_GET, [](AsyncWebServerRequest *request){  
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+    led_state_ctl = 2;
+  });
+
+  // ventilator state Button
+  
+  server.on("/b4_off", HTTP_GET, [](AsyncWebServerRequest *request){  
+    ventilator_state_ctl = 0;
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  server.on("/b4_on", HTTP_GET, [](AsyncWebServerRequest *request){  
+    ventilator_state_ctl = 1;
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  }); 
+  server.on("/b4_auto", HTTP_GET, [](AsyncWebServerRequest *request){  
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+    ventilator_state_ctl = 2;
+  });
+  
   // Start server
   server.begin();
 }
@@ -182,32 +321,64 @@ void loop(){
   if ((millisec - cycle_read_pre > cycle_read_period)) {
     cycle_read_pre = millisec; 
   
-    StaticJsonDocument<300> rxdoc;
+    StaticJsonDocument<rxbuffer> rxdoc;
     DeserializationError error = deserializeJson(rxdoc, nodemcu);
 
     if (error) {
-      if (debug_level == 2) {
+      if (debug_level >= 2) {
         Serial.print(F("deserializeJson() failed: "));
         Serial.println(error.f_str());       
       }
     }
       else {
-        if (rxdoc.isNull()) {       
+        if (rxdoc.isNull() || rxdoc.memoryUsage() < 284) {       
         } else {
           arduino_time_ms = rxdoc["time"];              // extract time from arduino   
-          TS01 = rxdoc["data"][0];                      // extract data
-          FS01 = rxdoc["data"][1];
-          dummy_state = rxdoc["state"][0];        
+          TS01_int = rxdoc["data"][0];                      // extract data
+          TS02_int = rxdoc["data"][1];
+          TS03_int = rxdoc["data"][2];
+          dutycycle_int = rxdoc["data"][3];
+          
+          TS01 = float(TS01_int)/10;
+          TS02 = float(TS02_int)/10;
+          TS03 = float(TS03_int)/10;      
+          dutycycle = float(dutycycle_int)/10; 
+             
+          dummy_state =       rxdoc["state"][0];        
+          pipevent_state =    rxdoc["state"][1];
+          led_state =         rxdoc["state"][2];
+          ventilator_state =  rxdoc["state"][3];
+
+          word_hour =         rxdoc["rtc_time"][0];
+          word_minute =       rxdoc["rtc_time"][1];
+         
           _reads++;
 
-          if (debug_level == 1) {
-            Serial.print("###READ### rx-buffer: "+String(rxdoc.size())+","+String(sizeof(rxdoc))+","+String(rxdoc.memoryUsage()));            // Debugging      
-            Serial.print("\n");             
+          if (debug_level >= 1) {           
             serializeJson(rxdoc, Serial);     
             Serial.print("\n");   
           }         
+          if (debug_level >= 2) {
+            Serial.print("###READ### rx-buffer: "+String(rxdoc.size())+","+String(sizeof(rxdoc))+","+String(rxdoc.memoryUsage()));            // Debugging      
+            Serial.print("\n");               
+          }    
       }
     }
+    
+// ############################ Buttons 
+
+    dummy_state_ctl_str       = state_ctl_txt(dummy_state_ctl);
+    pipevent_state_ctl_str    = state_ctl_txt(pipevent_state_ctl);
+    led_state_ctl_str         = state_ctl_txt(led_state_ctl);
+    ventilator_state_ctl_str  = state_ctl_txt(ventilator_state_ctl);
+
+// ############################ States from Arduino 
+
+    dummy_state_str       = state_txt(dummy_state);
+    pipevent_state_str    = state_txt(pipevent_state);
+    led_state_str         = state_txt(led_state);
+    ventilator_state_str  = state_txt(ventilator_state);
+       
   }
   
 // ############################ SEND 
@@ -215,54 +386,30 @@ void loop(){
   if ((millisec - cycle_send_pre > cycle_send_period)) {
     cycle_send_pre = millisec;
     
-    StaticJsonDocument<200> txdoc;
+    StaticJsonDocument<txbuffer> txdoc;
   
     txdoc["growbox"] = "to_Arduino";
     JsonArray state = txdoc.createNestedArray("switches");      // Add state array
     state.add(dummy_state_ctl);                                           // Put data in array
-  
+    state.add(pipevent_state_ctl);
+    state.add(led_state_ctl);
+    state.add(ventilator_state_ctl);
+     
     //Send data to Arduino
     serializeJson(txdoc, nodemcu);
 
     _sends++;
 
-    if (debug_level == 1) {  
-      Serial.print("###SEND### tx-buffer: "+String(txdoc.size())+","+String(sizeof(txdoc))+","+String(txdoc.memoryUsage()));  
-      Serial.print("\n");             
+    if (debug_level >= 1) {            
       serializeJson(txdoc, Serial);                               
       Serial.print("\n");
     }
+    if (debug_level >= 2) {  
+      Serial.print("###SEND### tx-buffer: "+String(txdoc.size())+","+String(sizeof(txdoc))+","+String(txdoc.memoryUsage()));  
+      Serial.print("\n");             
+    }
   }
-
-// ############################ Buttons 
-
-  switch (dummy_state_ctl) {
-    case 0:
-      dummy_state_ctl_str = "Manual OFF";
-      break;
-    case 1:
-      dummy_state_ctl_str = "Manual ON";
-      break;
-    case 2:
-      dummy_state_ctl_str = "AUTOMATIC";
-      break;  
-    default:
-      break;
-  }
-
-// ############################ States from Arduino 
-
-  switch (dummy_state) {
-    case 0:
-      dummy_state_str = "OFF";
-      break;
-    case 1:
-      dummy_state_str = "ON";
-      break;
-    default:
-      break;
-  }
-    
+   
   delay(1);
 }
   
